8924aa946486569c60655f1a41f5ccfd8ee08dac
Shourya Sengupta <shourya.sengupta@gmail.com>
2023-04-13 13:40:29 +0530

    adding minor changes

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -4,44 +4,37 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+
 const openai = new OpenAIApi(configuration);
 // you are a copywriter, you are the best one in the world you have to review the text written below and rewrite it to make it more witty and conversational
-const basePromptPrefix =
-  // 'Help me write lyrics in the style of Drake, Canadian Rapper\n'
-  'You are a copywriter, you are the best one in the world you have to review the text written below and rewrite it to make it more witty and conversational\n';
-const finalPromptPrefix = 'Take the lyrics below and generate 5 song titles:\n';
 
 const generateAction = async (req, res) => {
+  const {input, tonalities} = req.body
+  const basePromptPrefix =
+  `You are a copywriter, you are the best one in the world you have to review the text written below and rewrite it to make it more ${tonalities.join(' and ')}\nText:${input}\nOutput:`;
 
   let baseCompletion;
   try {
     // Run first prompt
-  baseCompletion = await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: `${basePromptPrefix}${req.body.input}`,
-    temperature: 0.7,
-    max_tokens: 2048,
-  });
+    baseCompletion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `${basePromptPrefix}`,
+      temperature: 0.7,
+      max_tokens: 2048,
+    });
+
+    console.log({baseCompletion})
+    if (!baseCompletion) {
+      throw new Error('Didnt get proper response')
+    }
+
+    const baseChoice = baseCompletion && baseCompletion.data.choices.pop();
+
+    res.status(200).json({ baseChoice });
   } catch (e) {
+    res.status(500).json({ errorMessage: e.message });
     console.log(e);
   }
-  
-  const baseChoice = baseCompletion.data.choices.pop();
-
-  // Run second prompt with prefix
-  // const finalPrompt = `${finalPromptPrefix}${req.body.input}${baseChoice.text}`;
-
-  // const prefixCompletion = await openai.createCompletion({
-  //   model: 'text-davinci-003',
-  //   prompt: finalPrompt,
-  //   temperature: 0.7,
-  //   max_tokens: 2048,
-  // });
-
-  // const finalChoice = prefixCompletion.data.choices.pop();
-
-  // res.status(200).json({ baseChoice, finalChoice });
-  res.status(200).json({ baseChoice });
 };
 
 export default generateAction;
